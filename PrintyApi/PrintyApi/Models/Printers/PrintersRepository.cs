62d2e656ce91c73607dda8edd57084cac5ed133e
Morasiu <moras2601@wp.pl>
2020-11-21 05:42:20 +0100

    More examples

--- PrintyApi/PrintyApi/Models/Printers/PrintersRepository.cs
+++ PrintyApi/PrintyApi/Models/Printers/PrintersRepository.cs
@@ -65,6 +65,100 @@ namespace PrintyApi.Models.Printers {
 					}
 				},
 				new Printer {
+					Name = "Drukarnia Joasia",
+					Email = "cyfra@gmail.com",
+					Latitude = 50.05351888215761, 
+					Longitude = 19.94331651530966,
+					PhoneNumber = "123456789",
+					ServiceTypes = new [] { ServiceType.Print2D },
+					Services = new List<PrintService> {
+						new PrintService {
+							Dimension = Dimension.TwoDimensional,
+							Color = PrintColor.BlackAndWhite,
+							PrintMaterial = PrintMaterial.StandardPaper,
+							Size = PrintSize.A1,
+							Price = 10m
+						},
+						new PrintService {
+							Dimension = Dimension.TwoDimensional,
+							Color = PrintColor.BlackAndWhite,
+							PrintMaterial = PrintMaterial.StandardPaper,
+							Size = PrintSize.A2,
+							Price = 8m
+						},
+						new PrintService {
+							Dimension = Dimension.TwoDimensional,
+							Color = PrintColor.BlackAndWhite,
+							PrintMaterial = PrintMaterial.StandardPaper,
+							Size = PrintSize.A3,
+							Price = 6
+						},
+						new PrintService {
+							Dimension = Dimension.TwoDimensional,
+							Color = PrintColor.BlackAndWhite,
+							PrintMaterial = PrintMaterial.StandardPaper,
+							Size = PrintSize.A4,
+							Price = 2
+						},
+						new PrintService {
+							Dimension = Dimension.TwoDimensional,
+							Color = PrintColor.BlackAndWhite,
+							PrintMaterial = PrintMaterial.StandardPaper,
+							Size = PrintSize.A5,
+							Price = 1
+						},
+						new PrintService {
+							Dimension = Dimension.TwoDimensional,
+							Color = PrintColor.Colored,
+							PrintMaterial = PrintMaterial.StandardPaper,
+							Size = PrintSize.A1,
+							Price = 20m
+						},
+						new PrintService {
+							Dimension = Dimension.TwoDimensional,
+							Color = PrintColor.Colored,
+							PrintMaterial = PrintMaterial.StandardPaper,
+							Size = PrintSize.A2,
+							Price = 18m
+						},
+						new PrintService {
+							Dimension = Dimension.TwoDimensional,
+							Color = PrintColor.Colored,
+							PrintMaterial = PrintMaterial.StandardPaper,
+							Size = PrintSize.A3,
+							Price = 16
+						},
+						new PrintService {
+							Dimension = Dimension.TwoDimensional,
+							Color = PrintColor.Colored,
+							PrintMaterial = PrintMaterial.StandardPaper,
+							Size = PrintSize.A4,
+							Price = 12
+						},
+						new PrintService {
+							Dimension = Dimension.TwoDimensional,
+							Color = PrintColor.Colored,
+							PrintMaterial = PrintMaterial.StandardPaper,
+							Size = PrintSize.A5,
+							Price = 4
+						},
+						new PrintService {
+							Dimension = Dimension.TwoDimensional,
+							Color = PrintColor.BlackAndWhite,
+							PrintMaterial = PrintMaterial.BoldPaper,
+							Size = PrintSize.A4,
+							Price = 2.50m
+						},
+					},
+					Address = new Address {
+						StreetNumber = "1",
+						HouseNumber = "2",
+						Street = "Straszewskiego", 
+						ZipCode = "31-101",
+						City = "Kraków"
+					},
+				},
+				new Printer {
 					Name = "Cyfra 7. Druk cyfrowy Solwent",
 					Email = "cyfra@gmail.com",
 					Latitude = 50.05656237616683,
@@ -86,6 +180,64 @@ namespace PrintyApi.Models.Printers {
 						Street = "Straszewskiego", 
 						ZipCode = "31-101",
 						City = "Kraków"
+					},
+				},
+				new Printer {
+					Name = "3Dx - wynajem drukarek",
+					Email = "druk@gmail.com",
+					Latitude = 50.0576688239084, 
+					Longitude = 19.938658445788747,
+					PhoneNumber = "123456789",
+					ServiceTypes = new [] { ServiceType.Print3D },
+					Address = new Address {
+						StreetNumber = "46",
+						Street = "Grodzka", 
+						ZipCode = "31-044",
+						City = "Kraków"
+					},
+					Services = new List<PrintService> {
+						new PrintService {
+							Dimension = Dimension.ThreeDimensional,
+							PrintMaterial = PrintMaterial.PlasticAbs,
+							Color = PrintColor.PlasticGray,
+							Size = PrintSize.To6Cm,
+							Price = 10m,
+						},
+						new PrintService {
+							Dimension = Dimension.ThreeDimensional,
+							PrintMaterial = PrintMaterial.PlasticAbs,
+							Color = PrintColor.PlasticGray,
+							Size = PrintSize.To9Cm,
+							Price = 15m,
+						},
+						new PrintService {
+							Dimension = Dimension.ThreeDimensional,
+							PrintMaterial = PrintMaterial.PlasticAbs,
+							Color = PrintColor.Orange,
+							Size = PrintSize.To6Cm,
+							Price = 10m,
+						},
+						new PrintService {
+							Dimension = Dimension.ThreeDimensional,
+							PrintMaterial = PrintMaterial.PlasticAbs,
+							Color = PrintColor.Orange,
+							Size = PrintSize.To9Cm,
+							Price = 15m,
+						},
+						new PrintService {
+							Dimension = Dimension.ThreeDimensional,
+							PrintMaterial = PrintMaterial.PlasticPla,
+							Color = PrintColor.PlasticGray,
+							Size = PrintSize.To6Cm,
+							Price = 8m,
+						},
+						new PrintService {
+							Dimension = Dimension.ThreeDimensional,
+							PrintMaterial = PrintMaterial.PlasticPla,
+							Color = PrintColor.PlasticGray,
+							Size = PrintSize.To9Cm,
+							Price = 13.50m,
+						}
 					}
 				},
 			};
